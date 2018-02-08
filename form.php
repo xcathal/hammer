27ccded1105d1076c74861e8875338454c88e526
xcathal <36042110+xcathal@users.noreply.github.com>
2018-02-08 14:39:54 +0100

    Wstępne
    Brak wersji nisko rozdzielczej.
    Brak trójkąta na mapie.

---
+++ form.php
@@ -0,0 +1,45 @@
+<?php
+
+// Tworzymy zmienną dla imienia i nazwiska
+$name = $_POST['name'];
+
+// Tworzymy zmienną dla adresu email
+$email = $_POST['email'];
+
+$subject = $_POST['subject'];
+
+// Tworzymy zmienną dla wiadomości
+$message = $_POST['message'];
+
+// Podajesz adres email z którego ma być wysłana wiadomość
+$odkogo = "biuro@79level.pl";
+
+// Podajesz adres email na który chcesz otrzymać wiadomość
+$dokogo = "narhir@o2.pl";
+
+// Podajesz tytuł jaki ma mieć ta wiadomość email
+$tytul = "Formularz kontaktowy z Hammer";
+
+// Przygotowujesz treść wiadomości
+$wiadomosc = "";
+$wiadomosc .= "Imie i nazwisko: " . $name . "\n";
+$wiadomosc .= "Email: " . $email . "\n";
+$wiadomosc .= "Subject: " . $subject . "\n";
+$wiadomosc .= "Wiadomość: " . $message . "\n";
+
+// Dodajemy UTF-8 do naglowka naszej wiadomości
+$naglowek = "";
+$naglowek .= "Od:" . $odkogo . " \n";
+$naglowek .= "Content-Type:text/plain;charset=utf-8";
+
+// Wysyłamy wiadomość
+$sukces = mail($dokogo, $tytul, $wiadomosc, $naglowek);
+
+// Przekierowywujemy na potwierdzenie
+if ($sukces){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+}
+?>
